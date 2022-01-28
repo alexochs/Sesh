89295e4f932800ec1bd3105525ef960df128cf5c
yori21 <mail@alexochs.de>
2022-01-28 14:54:49 +0100

    Add Redux to app
    Shitty old tutorial used, does the job for now but should be reworked to something newer...

--- SeshApp/index.js
+++ SeshApp/index.js
@@ -3,7 +3,23 @@
  */
 
 import {AppRegistry} from 'react-native';
+import React from 'react';
 import App from './App';
 import {name as appName} from './app.json';
+import {Provider} from 'react-redux';
 
-AppRegistry.registerComponent(appName, () => App);
+import configureStore from './store/configureStore';
+
+const store = configureStore();
+
+class RNRedux extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  }
+}
+
+AppRegistry.registerComponent(appName, () => RNRedux);
