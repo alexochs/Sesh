6494fbc3e5ff3e222aa408218f15763267f371b3
yori21 <mail@alexochs.de>
2022-01-30 02:42:22 +0100

    Add Firebase
    - Integrate Firebase into the project
    - Read/write to realtime database as example

--- SeshApp/App.js
+++ SeshApp/App.js
@@ -1,8 +1,16 @@
 import React, {Component} from 'react';
-import {SafeAreaView, StyleSheet, View, Button, Text} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Button,
+  Text,
+  Alert,
+} from 'react-native';
 import {connect} from 'react-redux';
-import {changeCount} from './actions/counts';
+import {changeCount} from './src/actions/counts';
 import {bindActionCreators} from 'redux';
+import database from '@react-native-firebase/database';
 
 class App extends Component {
   incrementCount() {
@@ -17,13 +25,24 @@ class App extends Component {
     actions.changeCount(count);
   }
 
+  fetchCount() {
+    let {actions} = this.props;
+    database()
+      .ref('/users/testuser/counter')
+      .once('value')
+      .then(snapshot => {
+        actions.changeCount(snapshot.val().counter.toString());
+      });
+  }
+
   render() {
     const {count} = this.props;
     return (
       <SafeAreaView styles={styles.container}>
-        <Button title="increment" onPress={() => this.incrementCount()} />
+        <Button title="Increment" onPress={() => this.incrementCount()} />
         <Text>{count}</Text>
-        <Button title="decrement" onPress={() => this.decrementCount()} />
+        <Button title="Decrement" onPress={() => this.decrementCount()} />
+        <Button title="Load Count from DB" onPress={() => this.fetchCount()} />
       </SafeAreaView>
     );
   }
--- SeshApp/src/reducers/countReducer.js
+++ SeshApp/src/reducers/countReducer.js
@@ -1,5 +1,8 @@
 import {COUNTER_CHANGE} from '../constants';
 
+import database from '@react-native-firebase/database';
+import {Alert} from 'react-native';
+
 const initialState = {
   count: 0,
 };
@@ -7,6 +10,9 @@ const initialState = {
 const countReducer = (state = initialState, action) => {
   switch (action.type) {
     case COUNTER_CHANGE:
+      database().ref('/users/testuser/counter').set({
+        counter: action.payload,
+      });
       return {
         ...state,
         count: action.payload,
